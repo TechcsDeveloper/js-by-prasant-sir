db3c83b125309567fdfadcac9830a82c52573263
BABULAL MANDAL <babulalmandal678@gmail.com>
2024-09-05 10:35:54 +0530

    Mytra Cart Calculation

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -1,1 +1,13 @@
-console.log("Number");
+//console.log("Number");
+let bagPrice = 15599;
+let trouserPrice = 2499;
+
+let discountBagPrice = ((bagPrice - (10/100) * bagPrice) - 272);
+console.log(discountBagPrice);
+
+let discountTrouserPrice = ((trouserPrice - (41/100) * 2499) -29);
+console.log(discountTrouserPrice);
+
+let cartValue = ((discountBagPrice + discountTrouserPrice)+(25+20));
+let finalValue = Math.round(cartValue);
+console.log(finalValue);
